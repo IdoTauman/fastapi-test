b5f3ac00f1d0fc0c1de68d10919860352f088d00
Ido Tauman <ido032009@gmail.com>
2023-11-28 19:42:37 +0200

    placeholder

--- app/endpoints/api_routers.py
+++ app/endpoints/api_routers.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 from app.endpoints.item import router as item_router
+from app.endpoints.user import router as user_router
 
 
 router = APIRouter()
 
 router.include_router(item_router, prefix="/item", tags=["Item"])
+router.include_router(user_router, prefix="/user", tags=["User"])
