ecfdf49d68b9b43f96d934d647d351642264a5ad
Ido Tauman <ido032009@gmail.com>
2023-12-02 18:19:21 +0200

    added item model, alembic migration

--- app/settings.py
+++ app/settings.py
@@ -1,9 +1,29 @@
+from typing import Optional, Any
+
 from pydantic_settings import BaseSettings
+from pydantic import validator, PostgresDsn
 
 
 class Settings(BaseSettings):
     WORDS_BASE_URL: str
     WORDS_API_KEY: str
+    POSTGRES_SERVER: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    SQLALCHEMY_DATABASE_URI: Optional[PostgresDsn] = None
+
+    @validator("SQLALCHEMY_DATABASE_URI", pre=True)
+    def assemble_db_connection(cls, v: Optional[str], values: dict[str, Any]) -> Any:
+        if isinstance(v, str):
+            return v
+        return PostgresDsn.build(
+            scheme="postgresql",
+            username=values.get("POSTGRES_USER"),
+            password=values.get("POSTGRES_PASSWORD"),
+            host=values.get("POSTGRES_SERVER"),
+            path=values.get('POSTGRES_DB') or '',
+        )
 
     class Config:
         case_sensitive = False
